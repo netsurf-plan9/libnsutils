2633e6f22b3de1030da5e65d5128672324ac9565
Vincent Sanders <vince@kyllikki.org>
2014-11-26 16:29:41 +0000

    add initial monotonic timer API

---
+++ include/nsutils/time.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2014 Vincent Sanders <vince@netsurf-browser.org>
+ *
+ * This file is part of libnsutils.
+ *
+ * Licensed under the MIT License,
+ *                http://www.opensource.org/licenses/mit-license.php
+ */
+
+/**
+ * \file
+ * Base64 encoding and decoding interface.
+ */
+
+#ifndef NSUTILS_TIME_H_
+#define NSUTILS_TIME_H_
+
+#include <nsutils/errors.h>
+
+/**
+ * Get a monotonically incrementing number of milliseconds.
+ *
+ * Obtain a count of elapsed time in milliseconds. Unlike gettimeofday this
+ * will continue linearly across time setting and not go backwards,
+ *
+ * \param current The current value of the counter.
+ * \return NSERROR_OK on success else error code.
+ */
+nserror nsu_nsu_getmonotonic_ms(uint64_t *current);
+ 
+#endif
---
+++ src/time.c
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2014 Vincent Sanders <vince@netsurf-browser.org>
+ *
+ * This file is part of libnsutils.
+ *
+ * Licensed under the MIT License,
+ *                http://www.opensource.org/licenses/mit-license.php
+ */
+
+/**
+ * \file
+ * Time operation implementation
+ */
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "nsutils/time.h"
+
+nserror nsu_nsu_getmonotonic_ms(uint64_t *current)
+{
+    return NSERROR_NOT_IMPLEMENTED;
+}
